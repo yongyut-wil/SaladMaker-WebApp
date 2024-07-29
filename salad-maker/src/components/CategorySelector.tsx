5251a17dd4e37e6ee654b29aa739051e05bb7b0b
YongyutWl <yongyut@iconframework.com>
2024-07-29 23:30:47 +0700

    Update CategorySelector.tsx

--- salad-maker/src/components/CategorySelector.tsx
+++ salad-maker/src/components/CategorySelector.tsx
@@ -19,7 +19,7 @@ const CategorySelector: React.FC = () => {
       {categories.map((category) => (
         <div
           key={category.name}
-          className="flex flex-col items-center bg-white rounded-lg p-4 shadow"
+          className="flex flex-col items-center bg-white rounded-lg p-4 shadow min-w-28"
         >
           <span className="text-2xl">{category.icon}</span>
           <span className="mt-2">{category.name}</span>
